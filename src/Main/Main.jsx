6e312ff2e0509eb65eb147eb874d9fc52656a29a
naeem <naeemislamjr182@gmail.com>
2023-10-25 22:06:42 +0600

    Basic layout with nav and footer

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -1,10 +1,14 @@
 import { Outlet } from "react-router-dom"
+import Navbar from "../Shared_Pages/Navbar/Navbar"
+import Footer from "../Shared_Pages/Footer/Footer"
 
 
 const Main = () => {
     return (
         <div>
+            <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,9 @@ import router from './Routes/Route.jsx';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
+  <div className=' max-w-7xl mx-auto'>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>,
+  </div>
 )

6178f9420a1cfff94b0f2297dd07450fedab1ac1
naeem <naeemislamjr182@gmail.com>
2023-10-25 23:37:30 +0600

    nav and Banner

--- src/Routes/Route.jsx
+++ src/Routes/Route.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Main/Main";
 import Home from "../Pages/Home/Home";
+import About from "../Pages/About/About";
 
 const router = createBrowserRouter([
     {
@@ -10,6 +11,10 @@ const router = createBrowserRouter([
             {
                 path: "/",
                 element: <Home />,
+            },
+            {
+                path: "/about",
+                element: <About />,
             },
         ]
     },
